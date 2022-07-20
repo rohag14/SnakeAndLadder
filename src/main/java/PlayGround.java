02aa9ab38e23f837d3345a1fcc091747dad9866a
rohita.int <rohita.int@media.net>
2022-07-20 17:37:26 +0530

    trying

---
+++ src/main/java/PlayGround.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayGround {
+    int row;
+    int column;
+    List<Cell> cellList = new ArrayList<>();
+
+    public void render() {
+
+    }
+}
